b99b7c97a333822791b098a9551796275e36980a
mitto nagisa <mitto@mittostar.info>
2012-05-28 12:57:49 +0900

    First Commit
    Signed-off-by: mitto <mitto.broadcast@gmail.com>

---
+++ Skype to Growl Notification/FormSetting.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Skype_to_Growl_Notification
+{
+    public partial class FormSetting : Form
+    {
+        public FormSetting()
+        {
+            InitializeComponent();
+        }
+    }
+}
