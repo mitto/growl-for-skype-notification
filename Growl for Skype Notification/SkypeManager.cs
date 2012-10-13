e12e3036873177a5bfa0de185003a4d490e7e1e8
mitto nagisa <mitto@mittostar.info>
2012-10-14 06:59:53 +0900

    add file "SkypeManager.cs"

---
+++ Growl for Skype Notification/SkypeManager.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Growl_for_Skype_Notification
+{
+    public class SkypeManager : SkypeManagerBase
+    {   
+        #region "変数"
+
+        #endregion
+
+        #region "定数"
+
+        #endregion
+
+        #region "メソッド"
+
+        #region "コンストラクタ"
+
+        #endregion
+
+        #endregion
+
+        #region "プロパティ"
+
+        #endregion
+
+
+    }
+}
