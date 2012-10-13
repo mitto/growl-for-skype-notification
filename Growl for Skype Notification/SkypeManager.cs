7de55250a55a8ee440c5e574f12e0dd31cf7f713
mitto nagisa <mitto@mittostar.info>
2012-10-13 22:19:16 +0900

    add StartingSkype method

--- Growl for Skype Notification/SkypeManager.cs
+++ Growl for Skype Notification/SkypeManager.cs
@@ -30,6 +30,16 @@ namespace Growl_for_Skype_Notification
 
         #endregion
 
+        /// <summary>
+        /// Skype自体を起動させるためのメソッド
+        /// </summary>
+        /// <param name="minized">最小化した状態で起動させるか</param>
+        /// <param name="nosplash">起動時にスプラッシュ画面を表示させるか</param>
+        public void StartingSkype(bool minized = false, bool nosplash = false)
+        {
+            skype.Client.Start(minized, nosplash);
+        }
+
         #endregion
 
         #region "プロパティ"
