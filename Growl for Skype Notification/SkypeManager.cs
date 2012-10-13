2b8ae4f3562a8a94961fe6b834c3f515b0c0782b
mitto nagisa <mitto@mittostar.info>
2012-10-13 23:23:36 +0900

    add GetAttachmentStatusMessage method

--- Growl for Skype Notification/SkypeManager.cs
+++ Growl for Skype Notification/SkypeManager.cs
@@ -40,6 +40,31 @@ namespace Growl_for_Skype_Notification
             skype.Client.Start(minized, nosplash);
         }
 
+        /// <summary>
+        /// TAttachmentStatusを渡すと該当する簡易メッセージを返すメソッド
+        /// </summary>
+        /// <param name="status">取得したいTAttachmentStatus</param>
+        /// <returns>アタッチ状態の簡易メッセージ</returns>
+        public static string GetAttachmentStatusMessage(TAttachmentStatus status)
+        {
+            switch (status)
+            {
+                case TAttachmentStatus.apiAttachSuccess:
+                    return "Skypeへの接続に成功しています。";
+                case TAttachmentStatus.apiAttachAvailable:
+                    return "Skypeへ接続できます。";
+                case TAttachmentStatus.apiAttachNotAvailable:
+                    return "Skype側の接続準備が完了していません。";
+                case TAttachmentStatus.apiAttachUnknown:
+                    return "原因不明で接続できません。";
+                case TAttachmentStatus.apiAttachPendingAuthorization:
+                    return "接続許可申請をSkype側にリクエスト済みです。";
+                case TAttachmentStatus.apiAttachRefused:
+                    return "Skypeとの接続が拒否されました。";
+            }
+            return "";
+        }
+
         #endregion
 
         #region "プロパティ"
