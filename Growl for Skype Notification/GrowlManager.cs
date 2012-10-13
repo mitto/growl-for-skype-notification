c7b1efdf4cdc233656b2f7e7d560b3e95bfbf64c
mitto nagisa <mitto@mittostar.info>
2012-10-13 20:34:04 +0900

    add GrowlManager.cs

---
+++ Growl for Skype Notification/GrowlManager.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Growl_for_Skype_Notification
+{
+    public class GrowlManager
+    {
+    }
+}
