932a5b15e2d2b09d2806ad12a789a6ba57ce79b9
mitto nagisa <mitto@mittostar.info>
2012-10-14 14:45:09 +0900

    add SkypeManager EventHandlers

--- Growl for Skype Notification/SkypeManager.cs
+++ Growl for Skype Notification/SkypeManager.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
+using SKYPE4COMLib;
+
 namespace Growl_for_Skype_Notification
 {
     public class SkypeManager : SkypeManagerBase
@@ -19,7 +21,38 @@ namespace Growl_for_Skype_Notification
 
         #region "コンストラクタ"
 
+        /// <summary>
+        /// デフォルトコンストラクタ
+        /// </summary>
+        public SkypeManager() : base()
+        {
+            skype.MessageStatus += skype_MessageStatus;
+            skype.OnlineStatus += skype_OnlineStatus;
+            skype.Reply += skype_Reply;
+            skype.UserMood += skype_UserMood;
+        }
+
         #endregion
+
+        private void skype_MessageStatus(ChatMessage pMessage, TChatMessageStatus Status)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void skype_OnlineStatus(User pUser, TOnlineStatus Status)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void skype_Reply(Command pCommand)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void skype_UserMood(User pUser, string MoodText)
+        {
+            throw new NotImplementedException();
+        }
 
         #endregion
 
