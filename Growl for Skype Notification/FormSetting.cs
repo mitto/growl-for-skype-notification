d052ef76a8582ce1ca9626d369688284c42520f2
mitto nagisa <mitto@mittostar.info>
2012-10-15 23:53:29 +0900

    最新バージョンの確認処理をUtilitiesクラスへ移動した

--- Growl for Skype Notification/FormSetting.cs
+++ Growl for Skype Notification/FormSetting.cs
@@ -54,6 +54,7 @@ namespace Growl_for_Skype_Notification
             linkLabelHome.LinkClicked += (sender, e) => Process.Start(linkLabelHome.Text);
 
             toolStripMenuItemExit.Click += (sender, e) => Application.Exit();
+            toolStripMenuItemCheckUpdate.Click += (sender, e) => Utilities.CheckNewDeployment();
 
             toolStripMenuItemAttachSkype.Click += (sender, e) => skypeManager.AttachSkype();
             toolStripMenuItemRegisterGrowl.Click += (sender, e) => skypeManager.GrowlRegister();
@@ -140,58 +141,7 @@ namespace Growl_for_Skype_Notification
 
         private void toolStripMenuItemGetAttachmentStatus_Click(object sender, EventArgs e)
         {
-            var status = skypeManager.AttachmentStatus;
-            MessageBox.Show(String.Format("{0}\n{1}", status.ToString(), SkypeManager.GetAttachmentStatusMessage(status)));
-        }
-
-        private void toolStripMenuItemCheckUpdate_Click(object sender, EventArgs e)
-        {
-            if (ApplicationDeployment.IsNetworkDeployed)
-            {
-                Boolean updateAvailable = false;
-                ApplicationDeployment ad = ApplicationDeployment.CurrentDeployment;
-
-                try
-                {
-                    updateAvailable = ad.CheckForUpdate();
-                }
-                catch (DeploymentDownloadException dde)
-                {
-                    MessageBox.Show("更新を確認中にエラーが発生しました。\nネットワークに繋がっているかを確認して再度お試しください。\n\nError:" + dde);
-                    return;
-                }
-                catch (InvalidDeploymentException ide)
-                {
-                    MessageBox.Show("アプリケーションがうまく配置されていない可能性があります。\n再インストールをお試しください。\n\nError: " + ide.Message);
-                    return;
-                }
-                catch (InvalidOperationException ioe)
-                {
-                    MessageBox.Show("すでに更新を確認中です。\n\nError: " + ioe.Message);
-                    return;
-                }
-
-                if (updateAvailable && MessageBox.Show(this, "最新版が利用できます。更新しますか？", "更新の確認", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == System.Windows.Forms.DialogResult.Yes)
-                {
-                    try
-                    {
-                        ad.Update();
-                    }
-                    catch (DeploymentDownloadException dde)
-                    {
-                        MessageBox.Show("更新をインストールできませんでした。\n更新サーバーがダウンしているかネットワークに接続していない可能性があります。\nネットワークに接続しているか確認して再度お試しください。\n\nError: " + dde.Message);
-                    }
-                    catch (TrustNotGrantedException tnge)
-                    {
-                        MessageBox.Show("更新をインストールできませんでした。\n\n\nError: " + tnge.Message);
-                    }
-                    if ((MessageBox.Show(this, "更新が完了しました。更新を有効にするにはアプリケーションを再起動する必要があります。再起動しますか？", "再起動の確認", MessageBoxButtons.YesNo, MessageBoxIcon.Question)) == DialogResult.Yes)
-                    {
-                        Application.Restart();
-                    }
-
-                }
-            }
+            MessageBox.Show(String.Format("{0}\n{1}", skypeManager.AttachmentStatus.ToString(), SkypeManager.GetAttachmentStatusMessage(skypeManager.AttachmentStatus)));
         }
 
         private void toolStripMenuItemMonitoringSkype_Click(object sender, EventArgs e)
--- Growl for Skype Notification/Utilities.cs
+++ Growl for Skype Notification/Utilities.cs
@@ -1,11 +1,67 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Deployment.Application;
 using System.Linq;
 using System.Text;
+using System.Windows.Forms;
 
 namespace Growl_for_Skype_Notification
 {
     public static class Utilities
     {
+        /// <summary>
+        /// 最新バージョンが発行されているかを調べ可能であれば更新を行うメソッド
+        /// 
+        /// * 現状はClickOnce版のみの対応
+        /// </summary>
+        public static void CheckNewDeployment()
+        {
+            if (ApplicationDeployment.IsNetworkDeployed)
+            {
+                bool updateAvailable = false;
+                var ad = ApplicationDeployment.CurrentDeployment;
+
+                try
+                {
+                    updateAvailable = ad.CheckForUpdate();
+                }
+                catch (DeploymentDownloadException dde)
+                {
+                    MessageBox.Show("更新を確認中にエラーが発生しました。\nネットワークに繋がっているかを確認して再度お試しください。\n\nError:" + dde);
+                    return;
+                }
+                catch (InvalidDeploymentException ide)
+                {
+                    MessageBox.Show("アプリケーションがうまく配置されていない可能性があります。\n再インストールをお試しください。\n\nError: " + ide.Message);
+                    return;
+                }
+                catch (InvalidOperationException ioe)
+                {
+                    MessageBox.Show("すでに更新を確認中です。\n\nError: " + ioe.Message);
+                    return;
+                }
+
+                if (updateAvailable && MessageBox.Show("最新版が利用できます。更新しますか？", "更新の確認", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == System.Windows.Forms.DialogResult.Yes)
+                {
+                    try
+                    {
+                        ad.Update();
+                    }
+                    catch (DeploymentDownloadException dde)
+                    {
+                        MessageBox.Show("更新をインストールできませんでした。\n更新サーバーがダウンしているかネットワークに接続していない可能性があります。\nネットワークに接続しているか確認して再度お試しください。\n\nError: " + dde.Message);
+                    }
+                    catch (TrustNotGrantedException tnge)
+                    {
+                        MessageBox.Show("更新をインストールできませんでした。\n\n\nError: " + tnge.Message);
+                    }
+                    if ((MessageBox.Show("更新が完了しました。更新を有効にするにはアプリケーションを再起動する必要があります。再起動しますか？", "再起動の確認", MessageBoxButtons.YesNo, MessageBoxIcon.Question)) == DialogResult.Yes)
+                    {
+                        Application.Restart();
+                    }
+
+                }
+            }
+        }
     }
 }
