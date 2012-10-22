829ee4b24effb2d578cc78f78261303b4a991d0e
mitto nagisa <mitto@mittostar.info>
2012-10-22 19:19:28 +0900

    LogType列挙体を追加した

--- Growl for Skype Notification/LogManager.cs
+++ Growl for Skype Notification/LogManager.cs
@@ -3,11 +3,30 @@ using System.IO;
 
 namespace Growl_for_Skype_Notification
 {
+    public enum LogType
+    {
+        Csv,
+        Tsv
+    }
+
     public static class LogManager
     {
         private static readonly string FileName = String.Format("log-{0}", DateTime.Now.ToString("yyyyMMddHHmmss"));
 
-        public static void WriteLineTsv(DateTime time, string type, string name, string id, string message)
+        public static void WriteLine(DateTime time, string type, string name, string id, string message, LogType logtype = LogType.Csv)
+        {
+            switch (logtype)
+            {
+                case LogType.Csv:
+                    WriteLineCsv(time, type, name, id, message);
+                    break;
+                case LogType.Tsv:
+                    WriteLineTsv(time, type, name, id, message);
+                    break;
+            }
+        }
+
+        private static void WriteLineTsv(DateTime time, string type, string name, string id, string message)
         {
             string date = time.ToLongDateString() + time.ToLongTimeString();
             string path = Path.Combine(SettingManager.LogFileDirectoryPath, FileName + ".tsv");
@@ -18,7 +37,7 @@ namespace Growl_for_Skype_Notification
             }
         }
 
-        public static void WriteLineCsv(DateTime time, string type, string name, string id, string message)
+        private static void WriteLineCsv(DateTime time, string type, string name, string id, string message)
         {
             string date = time.ToLongDateString() + time.ToLongTimeString();
             string path = Path.Combine(SettingManager.LogFileDirectoryPath, FileName + ".csv");
@@ -28,6 +47,5 @@ namespace Growl_for_Skype_Notification
                 writer.WriteLine("{0},{1},{2},{3}", date, type, name, message);
             }
         }
-
     }
 }
